f7624eb503a392bcf4871e45d2d45341983bdb16
Hackerl <liupan052997@gmail.com>
2016-05-07 10:36:00 +0800

    match

---
+++ match.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+#通过增广路径算法寻找最大双边匹配
+#迭代所有节点，开始探查，如果发现右边有没匹配的点，则中止循环进行匹配
+#forw生成可前进的路径，back生成后退的路径
+#左边迭代所有前进的路径，加入循环堆，循环到右边节点，寻找后退路径
+#直到推到左边，找到一个未匹配的右边节点，中止循环，更改这条路径上的所有匹配
+
+from itertools import chain
+
+def match(G,X,Y):
+    H=tr(G)
+    S,T,M=set(X),set(Y),set()
+    #左边节点集，右边节点集，已匹配集合
+    while S:
+        s=S.pop()
+        Q,P={s},{}
+        while Q:
+            u=Q.pop()
+            if u in T:
+                #如果u还没有在T中，说明没有被配对
+                T.remove(u)
+                #移除u
+                break
+            forw=(v for v in G[u] if (u,v) not in M)
+            back=(v for v in H[u] if (v,u) in M)
+            #前进及后退路径
+            for v in chain(forw,back):
+            #chian将两个列表组合迭代
+                if v in P:continue
+                P[v]=u
+                Q.add(v)
+        while u != s:
+            u,v=P[u],u
+            #迭代寻找到的路径边集
+            if v in G[u]:
+                M.add((u,v))
+                #如果v在左边，添加边，否则移除边
+            else:
+                M.remove((v,u))
+    return M
